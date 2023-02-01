b0168c8fc0cb0c3d1f60b4cedc0e4d83124bba38
Pawel Tatara <paweltatara45@gmail.com>
2023-02-01 18:22:11 +0100

    Homework Day 4

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string sex1 = "Kobieta";
+int age1 = 28;
+
+string name1 = "Ewa";
+int age2 = 33;
+
+string sex2 = "Mezczyzna";
+int age3 = 16;
+
+string name2 = "Pawel";
+int age4 = 40; 
+
+
+if (sex1 == "Kobieta" && age1 < 30)
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
+if (name1 == "Ewa" && age2 == 33)
+{
+    Console.WriteLine("Ewa lat 33");
+
+}
+if (sex2 == "Mezczyzna" && age3 < 18 )
+{
+    Console.WriteLine("Niepelnoletni Mezczyzna");
+
+}
+if (name2 == "Pawel" && age4 == 40)
+{
+    Console.WriteLine("Cos o mnie");
+}
