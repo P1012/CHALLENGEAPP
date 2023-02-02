0516867e6b7272b630657f415868ccfcea93dd80
Pawel Tatara <paweltatara45@gmail.com>
2023-02-02 20:51:40 +0100

    Homework day 5 - Number.

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,35 +1,74 @@
 ﻿
-using System;
+int number = 101283;
+string numberAsString = number.ToString();
+char[] letters = numberAsString.ToCharArray();
 
-string sex1 = "Kobieta";
-int age1 = 28;
-
-string name1 = "Ewa";
-int age2 = 33;
-
-string sex2 = "Mezczyzna";
-int age3 = 16;
-
-string name2 = "Pawel";
-int age4 = 40;
-
-
-if (sex1 == "Kobieta" && age1 < 30)
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0; 
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+foreach (char letter in letters)
 {
-    Console.WriteLine("Kobieta ponizej 30 lat");
+	if (letter == '0')
+	{
+		counter0++;
+
+	}
+	else if (letter == '1')
+	{
+		counter1++;
+	}
+	else if (letter == '2')
+	{
+		counter2++;
+	}
+	else if (letter == '3')
+	{
+		counter3++;
+	}
+	else if (letter == '4')
+	{
+		counter4++;
+	}
+	else if (letter == '5')
+	{
+		counter5++;
+	}
+	else if (letter == '6')
+	{
+		counter6++;
+	}
+	else if (letter == '7')
+	{
+		counter7++;
+	}
+	else if (letter == '8')
+	{
+		counter8++;
+	}
+	else if (letter == '9')
+	{
+		counter9++;
+	}
+	Console.WriteLine($" Skladowa liczby {number} to");
+	Console.WriteLine($"0 jest {counter0} ");
+	Console.WriteLine($"1 jest {counter1}");
+	Console.WriteLine($"2 jest {counter2}");
+	Console.WriteLine($"3 jest {counter3}");
+	Console.WriteLine($"4 jest {counter4} ");
+	Console.WriteLine($"5 jest {counter5}");
+	Console.WriteLine($"6 jest {counter6}");
+	Console.WriteLine($"7 jest {counter7} ");
+	Console.WriteLine($"8 jest {counter8}");
+	Console.WriteLine($"9 jest {counter9}");
 
 
-}
-else if (name1 == "Ewa" && age2 == 33)
-{
-    Console.WriteLine("Ewa lat 33");
 
-}
-else if (sex2 == "Mezczyzna" && age3 < 18)
-{
-    Console.WriteLine("Niepelnoletni Mezczyzna");
-}
-else if (name2 == "Pawel" && age4 == 40)
-{
-    Console.WriteLine("Cos o mnie");
-}
+}   
+
