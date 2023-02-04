34f65b7c247a77555a606b4835d5016a51e3d7b7
Pawel Tatara <paweltatara45@gmail.com>
2023-02-04 18:24:16 +0100

    Homework day 6

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,74 +1,58 @@
-﻿
-int number = 101283;
-string numberAsString = number.ToString();
-char[] letters = numberAsString.ToCharArray();
+﻿using ChallengeApp;
 
-int counter0 = 0;
-int counter1 = 0;
-int counter2 = 0;
-int counter3 = 0;
-int counter4 = 0;
-int counter5 = 0;
-int counter6 = 0; 
-int counter7 = 0;
-int counter8 = 0;
-int counter9 = 0;
-foreach (char letter in letters)
+
+
+Employee employee1 = new Employee("Ziutka", "Berbelucha", "33");
+Employee employee2 = new Employee("Henio", "Wyrwizabek" , "43");
+Employee employee3 = new Employee("Czesio", "WlaTcyMoch", "8");
+
+
+employee1.AddScore(5); 
+employee1.AddScore(4); 
+employee1.AddScore(9);
+employee1.AddScore(7);
+employee1.AddScore(1);
+
+employee2.AddScore(8);
+employee2.AddScore(3);
+employee2.AddScore(6); 
+employee2.AddScore(8); 
+employee2.AddScore(10);
+
+employee3.AddScore(8);
+employee3.AddScore(1); 
+employee3.AddScore(3); 
+employee3.AddScore(9);
+employee3.AddScore(7); 
+employee3.AddScore(9);
+
+List<Employee> employees = new List<Employee>()
 {
-	if (letter == '0')
-	{
-		counter0++;
 
-	}
-	else if (letter == '1')
-	{
-		counter1++;
-	}
-	else if (letter == '2')
-	{
-		counter2++;
-	}
-	else if (letter == '3')
-	{
-		counter3++;
-	}
-	else if (letter == '4')
-	{
-		counter4++;
-	}
-	else if (letter == '5')
-	{
-		counter5++;
-	}
-	else if (letter == '6')
-	{
-		counter6++;
-	}
-	else if (letter == '7')
-	{
-		counter7++;
-	}
-	else if (letter == '8')
-	{
-		counter8++;
-	}
-	else if (letter == '9')
-	{
-		counter9++;
-	}
-	Console.WriteLine($" Skladowa liczby {number} to");
-	Console.WriteLine($"0 jest {counter0} ");
-	Console.WriteLine($"1 jest {counter1}");
-	Console.WriteLine($"2 jest {counter2}");
-	Console.WriteLine($"3 jest {counter3}");
-	Console.WriteLine($"4 jest {counter4} ");
-	Console.WriteLine($"5 jest {counter5}");
-	Console.WriteLine($"6 jest {counter6}");
-	Console.WriteLine($"7 jest {counter7} ");
-	Console.WriteLine($"8 jest {counter8}");
-	Console.WriteLine($"9 jest {counter9}");
+    employee1 , employee2, employee3 ,
+
+};
 
 
 
-}   
+
+int maxResult = -1;
+Employee employeeWithMaxResult = null;
+
+foreach(var employee in employees)
+{
+    if (employee.Result > maxResult)
+    {
+        maxResult= employee.Result;
+        employeeWithMaxResult = employee;
+
+    }
+}
+
+Console.WriteLine("Pracownik z najwiekszym wynikiem to :");
+Console.WriteLine(employeeWithMaxResult.Name + "");
+Console.WriteLine(employeeWithMaxResult.Surname + "");
+Console.WriteLine(employeeWithMaxResult.Age + "lat ,");
+Console.WriteLine($"Wynik: {employeeWithMaxResult.Result}punktow");
+Console.WriteLine();
 
