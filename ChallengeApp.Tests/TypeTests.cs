2d9b311cd0031b5a8b02826a400f46a88f3028a9
Pawel Tatara <paweltatara45@gmail.com>
2023-02-14 17:00:37 +0100

    Homework Day 8

---
+++ ChallengeApp.Tests/TypeTests.cs
@@ -0,0 +1,54 @@
+﻿namespace ChallengeApp.Tests
+{
+    public class TypeTests
+    {
+        [Test]
+        public void GetEmployeesNameSurnameAndAgeEqualOrNotEqual()
+        {
+            //Arrange 
+            var employee1 = GetEmployee("Henio", "Kozka", "33");
+            var employee2 = GetEmployee("Helcia", "Waza", "45");
+            var employee3 = GetEmployee("Czesio", "Polmozg", "8");
+            var employee4 = GetEmployee("Czesio", "Polmozg", "8");
+            // Act
+
+
+            // Assert
+
+            Assert.AreNotEqual(employee1, employee2);
+            Assert.AreEqual(employee3.Age, employee4.Age);
+        }
+        private Employee GetEmployee(string name, string surname, string age)
+        {
+
+            return new Employee(name, surname, age);
+
+        }
+        [Test]
+        public void CompareNumbersEqualOrNotEqual()
+        {
+            int number1 = 6;
+            int number2 = 10;
+            int number3 = 9;
+            int number4 = 9;
+
+
+            Assert.AreNotEqual(number1, number2);
+            Assert.AreEqual(number3, number4);
+
+        }
+        [Test]
+        public void CompareVodkasVolumeEqualOrNotEqual()
+        {
+            float vodka1 = 1;
+            float vodka2 = 2;
+            double vodka3 = 3;
+            double vodka4 = 3;
+
+            Assert.AreNotEqual(vodka1, vodka2);
+            Assert.AreEqual(vodka4, vodka3);
+        }
+    }
+}
+
+
